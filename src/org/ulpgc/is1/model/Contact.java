1547be8a83a8f876ef398d31bb395672c5820f57
robin <robin36guillermo@gmail.com>
2025-10-08 01:46:37 +0100

    final

--- src/org/ulpgc/is1/model/Contact.java
+++ src/org/ulpgc/is1/model/Contact.java
@@ -1,16 +1,29 @@
 package org.ulpgc.is1.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Contact {
     private String telephone;
     private String email;
     private Address address;
+    private List<Member> members;
 
 
     public Contact(String telephone, String email, String street, int number, int floor, String city ) {
         this.telephone = telephone;
         this.email = email;
         this.address = new Address(street, number, floor, city);
+        this.members = new ArrayList<>();
+    }
+
+    public void addMember(Contact this) {
+        members.add(new Member( this));
+    }
+
+    public List<Member> getMembers() {
+        return members;
     }
 
     public String getEmail() {
